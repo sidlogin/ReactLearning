f659714ef39de02d5fe4f1b8920c53d4f514d33a
sidlogin <siddhartha.gupta@gmail.com>
2022-04-06 21:31:50 -0500

    Deleting unwanted code

--- Udemy- ReactJS/Reference Github Code/ReactJS_VideoGuides-master/portfolio/src/projects/evens-or-odds/actions/guess.js
+++
@@ -1,9 +0,0 @@
-import { SET_GUESS } from './types';
-
-export const setGuessEven = () => {
-  return { type: SET_GUESS, guess: 'even' };
-}
-
-export const setGuessOdd = () => {
-  return { type: SET_GUESS, guess: 'odd' };
-}

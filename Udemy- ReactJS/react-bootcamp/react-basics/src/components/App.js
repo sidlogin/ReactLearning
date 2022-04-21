d9f19f77f03bb736ced8b2dbe34ae858582b2317
sidlogin <siddhartha.gupta@gmail.com>
2022-04-21 14:19:31 -0500

    Committing

--- Udemy- ReactJS/react-bootcamp/react-basics/src/components/App.js
+++ Udemy- ReactJS/react-bootcamp/react-basics/src/components/App.js
@@ -1,12 +1,15 @@
-import './App.css'
 import ClassComponent from './basic-concepts/fundamentals/ClassComponent';
 import FunctionalComponent from './basic-concepts/fundamentals/FunctionalComponent'
+import Content from './ecosystems/Content';
+import Footer from './ecosystems/Footer';
+import Header from './ecosystems/Header';
 
 const App = () => {
   return (
-    <div className="App">
-      <FunctionalComponent />
-      <ClassComponent />
+    <div className='App'>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ Udemy- ReactJS/react-bootcamp/react-basics/src/components/ecosystems/Footer.js
@@ -0,0 +1,14 @@
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers'
+import React from 'react'
+
+const Footer = () => {
+  const today = new Date();
+
+  return (
+    <footer>
+        Copyright &copy; {today.getFullYear()}
+    </footer>
+  )
+}
+
+export default Footer

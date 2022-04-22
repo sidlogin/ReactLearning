2921f155566ab5209467f1a0e48e99bc04f5b07a
sidlogin <siddhartha.gupta@gmail.com>
2022-04-22 10:52:24 -0500

    Crud operation using Axios

--- Udemy- ReactJS/react-bootcamp/react-basics-router/src/App.js
+++ Udemy- ReactJS/react-bootcamp/react-basics-router/src/App.js
@@ -7,20 +7,41 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import NewPost from './components/NewPost';
 import PostPage from './components/PostPage';
+import EditPost from "./components/EditPost";
 import About from './components/About';
 import Missing from './components/Missing';
 import Home from './components/Home';
 import { format } from 'date-fns';
-
-import { getPosts } from "./data";
+import api from './api/posts';
 
 const App = () => {
-  const [posts, setPosts] = useState(getPosts() || []);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const navigateToPage = useNavigate();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        // console.log(response);
+        if (response) {
+          setPosts(response.data);
+        }
+      } catch (err) {
+        if(err.response) {
+          console.log(err.response.data, err.response.status, err.response.header);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      } finally {}
+    }
+    fetchPosts();
+  }, []);
 
   useEffect(() => {
     const filteredResults = posts.filter((post) =>
@@ -30,22 +51,47 @@ const App = () => {
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigateToPage('/');
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigateToPage('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   }
 
-  const handleDelete = (id) => {
-    const filteredPosts = posts.filter(post => post.id !== id);
-    setPosts(filteredPosts);
-    navigateToPage('/');
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+      setEditTitle('');
+      setEditBody('');
+      navigateToPage('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const filteredPosts = posts.filter(post => post.id !== id);
+      setPosts(filteredPosts);
+      navigateToPage('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   }
 
   return (
@@ -62,6 +108,16 @@ const App = () => {
                 postBody={postBody}
                 setPostBody={setPostBody}
                 handleSubmit={handleSubmit}
+              />
+            } />
+            <Route path="/edit/:id" element={
+              <EditPost
+                posts={posts}
+                handleEdit={handleEdit}
+                editTitle={editTitle}
+                setEditTitle={setEditTitle}
+                editBody={editBody}
+                setEditBody={setEditBody}
               />
             } />
             <Route path="/post/:id" element={
--- Udemy- ReactJS/react-bootcamp/react-basics-router/src/components/PostPage.js
+++ Udemy- ReactJS/react-bootcamp/react-basics-router/src/components/PostPage.js
@@ -14,6 +14,7 @@ const PostPage = ({posts, handleDelete}) => {
             <h2>{post.title}</h2>
             <p className='postDate'>{post.dateTime}</p>
             <p className='postBody'>{post.body}</p>
+            <Link to={`/edit/${post.id}`}><button className="editButton">Edit Post</button></Link> &nbsp;
             <button className='button' onClick={() => handleDelete(post.id)}>
               Delete Post
             </button>
